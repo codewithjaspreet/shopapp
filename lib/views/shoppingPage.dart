41130195bf16642b55f80c2bae1e9f70e3be0e40
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-05-23 15:41:59 +0530

    UI structure

--- lib/views/shoppingPage.dart
+++ lib/views/shoppingPage.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ShoppingPage extends StatelessWidget {
-  const ShoppingPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Card();
+                  })),
+          Text('Total Amount : '),
+          SizedBox(
+            height: 100,
+          ),
+        ],
+      ),
+    ));
   }
 }
