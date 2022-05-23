3f78144453c8999f6ed26b70cd1ab084b4c43a11
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-05-23 20:20:35 +0530

    product items data

---
+++ lib/models/Product Model.dart
@@ -0,0 +1,15 @@
+class Product {
+  late final int id;
+  late final String pName;
+  late final String pImg;
+  late final String pDesc;
+  late final double price;
+
+  Product({
+    required this.id,
+    required this.pName,
+    required this.pImg,
+    required this.pDesc,
+    required this.price,
+  });
+}
